ebcf61ac153910ce4a06c90b64ec6fbbc748df90
Quentin Vespero <quentinvespero@gmail.com>
2026-04-16 03:37:45 +0200

    update version

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import argparse
 import sys
 from pathlib import Path
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from rich.progress import Progress, BarColumn, TaskProgressColumn, TimeElapsedColumn, TaskID
 
